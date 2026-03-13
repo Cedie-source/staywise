56990ed71fc2c55c677687bc1fcd5e75ea8262e3
Cedie-source <pisalbondaniel24@gmail.com>
2026-03-14 02:40:06 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,16 +1,17 @@
 <?php
 date_default_timezone_set('Asia/Manila');
 
-$servername = getenv('MYSQLHOST') ?: 'localhost';
-$username   = getenv('MYSQLUSER') ?: 'root';
+$servername = getenv('MYSQLHOST')     ?: 'mysql.railway.internal';
+$username   = getenv('MYSQLUSER')     ?: 'root';
 $password   = getenv('MYSQLPASSWORD') ?: '';
-$dbname     = getenv('MYSQLDATABASE') ?: 'staywise';
-$port       = getenv('MYSQLPORT') ?: 3306;
+$dbname     = getenv('MYSQLDATABASE') ?: 'railway';
+$port       = (int)(getenv('MYSQLPORT') ?: 3306);
 
-$conn = new mysqli($servername, $username, $password, $dbname, (int)$port);
+mysqli_report(MYSQLI_REPORT_OFF);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
-    error_log("DB Error: " . $conn->connect_error);
+    error_log("StayWise DB connect error: " . $conn->connect_error);
     die("Database connection failed.");
 }
 
